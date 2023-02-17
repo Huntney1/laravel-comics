5c8d49dafd02a47af9e6dcb807a1e324a9c7691e
Huntney1 <devilwhiteone@hotmail.it>
2023-02-17 19:11:38 +0100

    input search e altri fix main fatti

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,64 +1,82 @@
 <header>
-   <section class="header-aside">
-       <div class="container">
-           <div class="row">
-               <div class="col-12">
-                   <ul class="m-0">
-                       <li>
-                           <a href="#">DC POWER&#8480; VISA&#174;</a>
-                       </li>
-                       <li>
-                           <a href="#">ADDITIONAL DC SITES</a>
-                       </li>
-                   </ul>
-               </div>
-           </div>
-       </div>
-   </section>
+    <section class="header-aside">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <ul class="m-0">
+                        <li>
+                            <a href="#">DC POWER&#8480; VISA&#174;</a>
+                        </li>
+                        <li>
+                            <a href="#">ADDITIONAL DC SITES</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 
-   <nav class="navbar navbar-expand-lg bg-body-tertiary">
-       <div class="container">
-           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-               <span class="navbar-toggler-icon"></span>
-           </button>
-           <a class="navbar-brand" href="{{route('comics')}}">
-               <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="Logo" class="d-block m-auto logo-img">
-           </a>
-           <div class="collapse navbar-collapse nav-list" id="navbarSupportedContent">
-               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                   <li class="nav-item">
-                       <a class="nav-link fw-bold" href="#">CHARACTERS</a>
-                   </li>
-                   <li class="nav-item">
-                       <a class="nav-link fw-bold active" aria-current="page" href="#">COMICS</a>
-                   </li>
-                   <li class="nav-item">
-                       <a class="nav-link fw-bold" href="#">MOVIES</a>
-                   </li>
-                   <li class="nav-item">
-                       <a class="nav-link fw-bold " href="#">TV</a>
-                   </li>
-                   <li class="nav-item">
-                       <a class="nav-link fw-bold" href="#">GAMES</a>
-                   </li>
-                   <li class="nav-item">
-                       <a class="nav-link fw-bold" href="#">COLLECTIBLES</a>
-                   </li>
-                   <li class="nav-item">
-                       <a class="nav-link fw-bold" href="#">VIDEOS</a>
-                   </li>
-                   <li class="nav-item">
-                       <a class="nav-link fw-bold" href="#">FANS</a>
-                   </li>
-                   <li class="nav-item">
-                       <a class="nav-link fw-bold" href="#">NEWS</a>
-                   </li>
-                   <li class="nav-item">
-                       <a class="nav-link fw-bold" href="#">SHOP</a>
-                   </li>
-           </div>
-       </div>
-   </nav>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="{{ route('comics') }}">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo"
+                    class="d-block m-auto logo-img">
+            </a>
+            <div class="collapse navbar-collapse nav-list" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">CHARACTERS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold border-bottom border-primary active" aria-current="page" href="#">COMICS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">MOVIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold " href="#">TV</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">GAMES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">COLLECTIBLES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">VIDEOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">FANS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">NEWS</a>
+                    </li>
+                </ul>
+                <div class="dropdown">
+                    <button class="btn dropdown-toggle font-Roboto" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false" style="border: none">
+                        Shop
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+                <form class="d-flex placeorder-form" role="search">
+                    <input class="form-control me-2 bi bi-search  border-bottom border-primary" type="search" placeholder="Search" aria-label="Search">
+                    <button class="placeorder-search-btn" type="submit">
+                      <i class="bi bi-search"></i>
+                    </button>
+                  </form>
+            </div>
+        </div>
+    </nav>
 </header>
 
 
@@ -132,4 +150,3 @@
     </div>
 </header>
  --}}
-
