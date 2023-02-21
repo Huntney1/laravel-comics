3d36ba1c328eafd3bfabcbdb0775d225a5910867
Huntney1 <devilwhiteone@hotmail.it>
2023-02-21 22:28:03 +0100

    fix css

--- resources/views/detail_fumetto.blade.php
+++ resources/views/detail_fumetto.blade.php
@@ -1,23 +1,27 @@
 @extends('layout.app')
 
 @section('main-content')
-<div class="jumbotron"></div>
+    <div class="jumbotron"></div>
     <div class="bg-blue"></div>
 
-    <div class="container">
-        <div class="img_cont">
-            <img src="{{$single['thumb']}}" alt="img-sing">
-            <p>{{ $single['type']}}</p>
+    <div class="body_sing">
+
+        <div class="container">
+            <div class="img_cont">
+                <img src="{{ $single['thumb'] }}" alt="img-sing">
+                <p>{{ $single['type'] }}</p>
+            </div>
+
         </div>
 
-    </div>
-
-    <div class="container d-flex">
-        <div class="row">
-            <div>
-                <h1 class="title">{{ $single['title'] }}</h1>
+        <div class="container d-flex">
+            <div class="row">
+                <div>
+                    <h1 class="title">{{ $single['title'] }}</h1>
+                </div>
             </div>
         </div>
     </div>
+
 
 @endsection
